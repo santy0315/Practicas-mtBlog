76b2904afd8fec9583c87f00d081002bda19ef61
santy0315 <banquezzapatasantiago@gmail.com>
2022-09-19 23:32:27 -0500

    Commit page About

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ import { PostComponent } from './component/posts/post/post.component';
 const routes: Routes = [
   {path : '', redirectTo: '/home', pathMatch:'full'},
   { path: 'home', loadChildren: () => import('./component/pages/home/home.module').then(m => m.HomeModule) },
-  { path: 'post/:id', component: PostComponent }];
+  { path: 'post/:id', component: PostComponent },
+  { path: 'about', loadChildren: () => import('./component/pages/about/about.module').then(m => m.AboutModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
