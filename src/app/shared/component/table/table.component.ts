998a97151a4625857c83c9cf0d58ec2f58e5b45f
santy0315 <banquezzapatasantiago@gmail.com>
2022-10-06 04:43:33 -0500

    table data firebase

--- src/app/shared/component/table/table.component.ts
+++ src/app/shared/component/table/table.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { MatPaginator } from '@angular/material';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
+import { PostService } from '../../../component/posts/post.service';
+import { PostI } from '../../models/post.interface';
 
 export interface PeriodicElement {
   name: string;
@@ -10,18 +12,6 @@ export interface PeriodicElement {
   symbol: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
 
 
 @Component({
@@ -29,21 +19,44 @@ const ELEMENT_DATA: PeriodicElement[] = [
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent implements OnInit {
+export class TableComponent implements OnInit, AfterViewInit {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  displayedColumns: string[] = ['titlePost', 'tagsPost', 'actions'];
+  dataSource = new MatTableDataSource();
    @ViewChild(MatPaginator, {static: true})paginator: MatPaginator;
    @ViewChild(MatSort, {static: true})sort: MatSort;
 
-  constructor() { }
+  constructor(private postSvc: PostService) { }
 
   ngOnInit() {
+    this.postSvc
+      .getAllPosts()
+      .subscribe(posts => (this.dataSource.data = posts));
+  }
+
+  ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
+
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+
+  onEditPost(post: PostI) {
+    console.log('Edit post', post);
+    
+  }
+
+  onDeletePost(post: PostI) {
+    console.log('Delete post', post);
+    
+  }
+
+  onNewPost() {
+    console.log('New post');
+    
   }
 
 
